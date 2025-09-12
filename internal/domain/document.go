d461704e317cd2822ac8326fb29d7cb759ff8ad8
Алишер Platform <@a.dautov>
2025-09-13 03:05:46 +0500

    Fix dirty table error

--- internal/domain/document.go
+++ internal/domain/document.go
@@ -9,6 +9,7 @@ type Document struct {
 	ID                uuid.UUID  `json:"id"`
 	Type              string     `json:"type"`
 	EmployeeID        uuid.UUID  `json:"employeeId"`
+	TemplateID			uuid.UUID        `json:"templateId"`
 	TemplateVersionID int        `json:"templateVersionId"`
 	Number            string     `json:"number"`
 	Date              time.Time  `json:"date"`
