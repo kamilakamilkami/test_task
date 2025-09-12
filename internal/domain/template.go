0621fe8697112082388d0786a2e7608d27cc547a
Алишер Platform <@a.dautov>
2025-09-13 01:35:32 +0500

    Create Domain Template & Its Migrations

---
+++ internal/domain/template.go
@@ -0,0 +1,19 @@
+package domain
+
+import (
+	"time"
+	"github.com/google/uuid"
+)
+
+type Template struct {
+	ID          uuid.UUID `json:"id"`
+	Name        string    `json:"name"`
+	Type        string    `json:"type"`   
+	Engine      string    `json:"engine"` 
+	Body        string    `json:"body"`   
+	Placeholders string   `json:"placeholders"` 
+	Version     int       `json:"version"`
+	IsActive    bool      `json:"isActive"`
+	CreatedBy   uuid.UUID `json:"createdBy"`
+	CreatedAt   time.Time `json:"createdAt"`
+}
